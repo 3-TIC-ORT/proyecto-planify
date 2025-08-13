8d1ee34f75d80214c991514ecf0d747a20e80873
LeonelGunther <49980496@est.ort.edu.ar>
2025-08-13 10:13:54 -0300

    modifique para que utilize usuaro en vez de email

--- Frontend/Inicio de seción/Pantaya de inicio de seción.js
+++ Frontend/Inicio de seción/Pantaya de inicio de seción.js
@@ -2,19 +2,18 @@ let form = document.getElementById('loginForm');
 let message = document.getElementById('message');
 
 form.addEventListener('submit', function(Preventivo) {
-  Preventivo.preventDefault(); // Evita el envío del formulario
+  Preventivo.preventDefault(); 
 
-  let email = document.getElementById('email').value.trim();
+  let Text = document.getElementById('text').value.trim();
   let password = document.getElementById('password').value.trim();
 
-  // Validación básica
-  if (email === "" || password === "") {
+ 
+  if (Text === "" || password === "") {
     message.innerHTML = "<p class='error'>Por favor completa todos los campos.</p>";
-    return;
   }
 
-  // Simulación de autenticación
-  else if (email === "usuario@ejemplo.com" && password === "123456") {
+
+  else if (Text === "papastriglio" && password === "123456") {
     message.innerHTML = "<p class='success'>Inicio de sesión exitoso 🎉</p>";
   } else {
     message.innerHTML = "<p class='error'>Correo o contraseña incorrectos.</p>";
