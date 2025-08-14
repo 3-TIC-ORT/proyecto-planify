9c9c193f7047e4235c257e7cfcf3ce04937ef50a
Dantestriglio <Dantestriglio@gmail.com>
2025-08-14 10:34:22 -0300

    Crear cuenta

---
+++ Frontend/Crear cuenta/Planify java script.js
@@ -0,0 +1,25 @@
+document.getElementById("registroForm").addEventListener("submit", function(e) {
+    e.preventDefault(); 
+
+    let usuario = document.getElementById("usuario").value.trim();
+    let password = document.getElementById("password").value;
+    let confirmar = document.getElementById("confirmar").value;
+    let mensaje = document.getElementById("mensaje");
+
+    if (usuario === "" || password === "" || confirmar === "") {
+        mensaje.textContent = "Todos los campos son obligatorios";
+        mensaje.className = "error";
+        return;
+    }
+
+    if (password !== confirmar) {
+        mensaje.textContent = "Las contraseñas no coinciden";
+        mensaje.className = "error";
+        return;
+    }
+
+    mensaje.textContent = "Cuenta creada con éxito ✅";
+    mensaje.className = "success";
+
+    
+});
