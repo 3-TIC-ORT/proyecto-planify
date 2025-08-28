d154aa9153af6a053fb6b77a281c3e2a5f023704
LeonelGunther <49980496@est.ort.edu.ar>
2025-08-28 10:36:39 -0300

    No me anda ponerle un grafico a la pantalla de gastos

--- Frontend/Pantalla de gastos/pantalla de gastos.js
+++ Frontend/Pantalla de gastos/pantalla de gastos.js
@@ -1,10 +1,10 @@
 var options = {
     chart: {
-      type: 'line'
+      type: 'bar'
     },
     series: [{
       name: 'sales',
-      data: [30,40,35,50,49,60,70,91,125]
+      data: [30,40,45,50,49,60,70,91,125]
     }],
     xaxis: {
       categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
