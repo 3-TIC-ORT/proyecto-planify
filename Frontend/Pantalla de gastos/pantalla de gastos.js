21fab63ca09996e35479888c90424b6e566fe062
LeonelGunther <49980496@est.ort.edu.ar>
2025-09-04 11:59:58 -0300

    tratando de poner un gráfico

--- Frontend/Pantalla de gastos/pantalla de gastos.js
+++ Frontend/Pantalla de gastos/pantalla de gastos.js
@@ -1,12 +1,14 @@
 
-
+let valoresG=[]
+let valoresI=[]
+let fechas =[]
 let chart; // variable global para guardar el gráfico
 
 document.getElementById("btnGenerar").addEventListener("click", () => {
   // Obtener valores ingresados
-  let n1 = parseFloat(document.getElementById("Texto de gastos").value) || 0;
-  let n2 = parseFloat(document.getElementById("Texto de ingresos").value) || 0;
-
+  let gasto = parseFloat(document.getElementById("Texto de gastos").value) || 0;
+  let ingreso = parseFloat(document.getElementById("Texto de ingresos").value) || 0;
+ papastriglio
   // Contexto del canvas
   let ctx = document.getElementById("miGrafico").getContext("2d");
 
@@ -17,26 +19,61 @@ document.getElementById("btnGenerar").addEventListener("click", () => {
 
   // Crear gráfico nuevo
   chart = new Chart(ctx, {
-    type: "pie", // podés cambiar a "line", "pie", etc.
+    type: "bar", // podés cambiar a "line", "pie", etc.
     data: {
       labels: ["ingresos", "gastos"],
       datasets: [{
         label: "Valores ingresados",
-        data: [n1, n2],
+        data: [ingreso, gasto],
         backgroundColor: ["#36A2EB", "#FF6384"], // colores
         borderColor: ["#1E88E5", "#E53935"],
-        borderWidth: 2
+        borderWidth: 1
       }]
-    },
-    options: {
+  },
+  options: {
       responsive: true,
-      
-    }
-  });
-  if (isNaN(n1) || isNaN(n2)) {
+      scales: {
+          y: { beginAtZero: true }
+      }
+  }
+});
+  if (isNaN(ingreso) || isNaN(gasto)) {
     alert("Por favor, introduce valores numéricos válidos para ingresos y gastos.");
     return;
     }
-  let promedio = (n1 + n2) / 2;
+  let promedio = (ingreso - gasto);
   document.getElementById("promedio").textContent = "El balance es:" + promedio;
 });
+
+ocument.getElementById("btnGenerar").addEventListener("click", () => { 
+   let = parseFloat(document.getElementById("Texto de gastos").value) || 0  
+   let ingre = parseFloat(document.getElementById("Texto de ingresos").value) || 0;
+
+  // Contexto del canvas
+  let ctx = document.getElementById("elGrafico").getContext("2d");
+
+  // Si ya hay un gráfico, lo destruyo antes de crear otro
+  if (chart) {
+    chart.destroy();
+  }
+
+  // Crear gráfico nuevo
+  chart = new Chart(ctx, {
+    type: "bar", // podés cambiar a "line", "pie", etc.
+    data: {
+      labels: ["ingresos", "gastos"],
+      datasets: [{
+        label: "Valores ingresados",
+        data: [ingreso, gasto],
+        backgroundColor: ["#36A2EB", "#FF6384"], // colores
+        borderColor: ["#1E88E5", "#E53935"],
+        borderWidth: 1
+      }]
+  },
+  options: {
+      responsive: true,
+      scales: {
+          y: { beginAtZero: true }
+      }
+  }
+  });
