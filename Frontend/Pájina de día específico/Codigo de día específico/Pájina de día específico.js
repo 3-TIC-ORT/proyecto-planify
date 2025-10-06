0404a7e012b4d4c1b5af0326fa13e81cf4c984d5
LeonelGunther <49980496@est.ort.edu.ar>
2025-10-06 10:39:07 -0300

    nfnoo0ooooooo entienndo

--- Frontend/Pájina de día específico/Codigo de día específico/Pájina de día específico.js
+++ Frontend/Pájina de día específico/Codigo de día específico/Pájina de día específico.js
@@ -3,9 +3,18 @@
   let url = new URLSearchParams(window.location.search);
   let fecha = url.get("fecha");
   let numero = url.get("numero")
+  let tarea = [
+    examen, Tarea, reunion,
+  ]
+  let info = document.querySelector('examen');
   
   let contenedor = document.getElementById("contenido");
   if (fecha) {
     contenedor.innerText = fecha + numero;
-
   }
+
+function crearcuadradoexamen(info){
+  a = document.createElement('div');
+  a.id = parametro.id || `cuadrado-dinamico-${Date.now()}`; // Un ID único
+  a.classList.add('cuadrado'); // Añadir la clase CSS
+}
