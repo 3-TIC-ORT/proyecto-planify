03f8f1e4479a6bf80a0fe2492e724da0c1d0e3cd
LeonelGunther <49980496@est.ort.edu.ar>
2025-10-09 10:42:12 -0300

    jhfkvfyukuftfyu

--- Frontend/Pájina de día específico/Codigo de día específico/Pájina de día específico.js
+++ Frontend/Pájina de día específico/Codigo de día específico/Pájina de día específico.js
@@ -3,18 +3,22 @@
   let url = new URLSearchParams(window.location.search);
   let fecha = url.get("fecha");
   let numero = url.get("numero")
-  let tarea = [
-    examen, Tarea, reunion,
-  ]
-  let info = document.querySelector('examen');
-  
-  let contenedor = document.getElementById("contenido");
+
   if (fecha) {
     contenedor.innerText = fecha + numero;
   }
 
-function crearcuadradoexamen(info){
-  a = document.createElement('div');
-  a.id = parametro.id || `cuadrado-dinamico-${Date.now()}`; // Un ID único
-  a.classList.add('cuadrado'); // Añadir la clase CSS
-}
+ let tarea =  {
+    examen : "tarea de Hardwar" ,
+    pueba: "tarea de mate"
+  }
+
+
+  tareas.forEach( tarea => {
+    
+    const titulo = document.createElement("h2")
+    titulo.textContent = examen
+    
+    tarjeta.appendChild(titulo)
+    cuadrado-tareas.appendChild(tarjeta)
+    });
