d632422c21dff1adab96c68dc257e7bde8095b79
Dantestriglio <Dantestriglio@gmail.com>
2025-10-20 14:10:33 -0300

    ponemos imagenes del menu lateral a todas las pantallas

--- Frontend/notas/notas.js
+++ Frontend/notas/notas.js
@@ -16,6 +16,36 @@
     function borrarNota() {
       localStorage.removeItem("notaGuardada");
       document.getElementById("texto").value = "";
-      alert("🗑️ Nota borrada");
+      alert(" Nota borrada");
     }
+
+    const botonmenu = document.querySelector(".menudeplegable");
+    const menulateral = document.querySelector(".menulateral");
+    botonmenu.addEventListener("click", () => {
+      menulateral.classList.toggle("activo");
+    });
+    
+    
+    const gastos = document.getElementById("gastos")
+    const tareas = document.getElementById("tareas")
+    const cerrarsesion = document.getElementById("cerrarsesion")
+    const notas = document.getElementById("notas")
+    
+    cerrarsesion.addEventListener("click", () => {
+      window.location.href ="C:\Users/50088774/Desktop/proyecto-planify/Frontend/crear cuenta/crear-cuenta.html"
+    });
+    
+    tareas.addEventListener("click", () => {
+      window.location.href ="C:/Users/50088774/Desktop/proyecto-planify/Frontend/eventos/eventos.html"
+    });
+    
+    gastos.addEventListener("click", () => {
+      window.location.href ="C:/Users/50088774/Desktop/proyecto-planify/Frontend/Pantalla de gastos/Pantalla de gastos.html"
+    });
+    
+    notas.addEventListener("click", () => {
+      window.location.href ="C:/Users/50088774/Desktop/proyecto-planify/Frontend/notas/notas.html"
+    });
+    
+    
  
