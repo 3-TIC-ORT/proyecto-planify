ba9762a95f0ace9f0bb8858de7aa87ab4ac72c8e
matias-sciarra <Matiassciarra1@gmail.com>
2025-10-29 14:09:39 -0300

    archivo planify.js listo, falta descarcagar soquetic, con gitignores y probar app.

--- Backend/Planify.js
+++ Backend/Planify.js
@@ -1,8 +1,22 @@
-// Este archivo va a servoioir para conectar todo dsp
-
-import { registrarUsuario, loginUsuario } from "./usuarios.js";
+import { subscribeGETEvent, subscribePOSTEvent, startServer } from "soquetic";
+import { signup, login, logout, obtenerUsuarioActivo } from "./usuarios.js";
 import { guardarNota, obtenerNotas } from "./notas.js";
 import { guardarEvento, obtenerEventos } from "./eventos.js";
-import { guardarGastos, obtenerGastos } from "./gastos.js";
+import { guardarGasto, obtenerGastos } from "./gastos.js";
 
+// -------------------- GET Events --------------------
+subscribeGETEvent("obtenerNotas", () => obtenerNotas());
+subscribeGETEvent("obtenerEventos", () => obtenerEventos());
+subscribeGETEvent("obtenerGastos", () => obtenerGastos());
+subscribeGETEvent("usuarioActivo", () => obtenerUsuarioActivo());
 
+// -------------------- POST Events --------------------
+subscribePOSTEvent("signup", (data) => signup(data.nombre, data.contra));
+subscribePOSTEvent("login", (data) => login(data.nombre, data.contra));
+subscribePOSTEvent("logout", () => logout());
+subscribePOSTEvent("guardarNota", (data) => guardarNota(data.contenido));
+subscribePOSTEvent("guardarEvento", (data) => guardarEvento(data.nombre, data.tipo, data.importancia, data.extra));
+subscribePOSTEvent("guardarGasto", (data) => guardarGasto(data.tipo, data.monto, data.fecha));
+
+// -------------------- Start server --------------------
+startServer(3000, true);
