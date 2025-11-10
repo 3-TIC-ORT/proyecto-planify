81a489c83ad1577d264a93d1b071364707eafad4
matias-sciarra <Matiassciarra1@gmail.com>
2025-11-10 15:49:25 -0300

    Notas terminado, falta eventos y gastos

--- Backend/Planify.js
+++ Backend/Planify.js
@@ -1,11 +1,12 @@
 import { subscribeGETEvent, subscribePOSTEvent, startServer } from "soquetic";
 import { login, signup } from "./usuarios.js";
-import { guardarnota } from "./notas.js";
+import { borrarnota, guardarnota } from "./notas.js";
 
 
 subscribePOSTEvent("signup", signup);
 subscribePOSTEvent("login", login);
-subscribePOSTEvent("guardarnota,", guardarnota);
+subscribePOSTEvent("guardarnota", guardarnota);
+subscribePOSTEvent("borrarnota" , borrarnota);
 
 
 startServer(3000, true);
--- Backend/notas.js
+++ Backend/notas.js
@@ -1,4 +1,5 @@
-import fs from "fs";
+import fs, { read, readFileSync } from "fs";
+import { networkInterfaces } from "os";
 
 
 export function guardarnota(data) {
@@ -6,14 +7,33 @@ export function guardarnota(data) {
 
   try {
   let notas = [];
+  notas = fs.readFileSync('notas.json' , 'utf-8')
   notas = fs.writeFileSync('notas.json', JSON.stringify(usuarios, null, 2));
     notas.push(data)
 
   return {exito: true, mensaje: "Nota creada con exito"}
 
 } 
-catch(Error) {
+catch(error) {
   console.error("Error al Guardar Nota:", Error);
   return {exito: false, mensaje: "Error al procesar el archivo"}
+  }
+}
+
+export function borrarnota(){
+
+try{
+  data = fs.readFileSync('notas.json' , 'utf-8') 
+  let notas = JSON.parse(data)
+
+  if (notas.length > 0) {
+    notas.pop();
+    fs.writeFileSync('notas.json' , JSON.stringify(notas, null, 2));
+    return {exito: true , mensaje: "Nota mas reciente eliminada con exito."};
+  } else {
+    return {exito: false , mensaje: "No hay notas para eliminar."}
+  }
+} catch(error) {
+return {exito: false, mensaje: "Error al borrar la nota." , error: error.mensaje};
   }
 }
