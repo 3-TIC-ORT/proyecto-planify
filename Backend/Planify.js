1367f8741fcada6ab8b6c4da176e5165de093e7e
matias-sciarra <Matiassciarra1@gmail.com>
2025-11-10 12:01:53 -0300

    Teerminada una variabke de notas

--- Backend/Planify.js
+++ Backend/Planify.js
@@ -1,9 +1,12 @@
 import { subscribeGETEvent, subscribePOSTEvent, startServer } from "soquetic";
 import { login, signup } from "./usuarios.js";
+import { guardarnota } from "./notas.js";
 
 
 subscribePOSTEvent("signup", signup);
-subscribePOSTEvent("login", login)
+subscribePOSTEvent("login", login);
+subscribePOSTEvent("guardarnota,", guardarnota);
+
 
 startServer(3000, true);
 
--- Backend/notas.js
+++ Backend/notas.js
@@ -1,50 +1,19 @@
 import fs from "fs";
-import { obtenerUsuarioActivo } from "./usuarios.js";
 
-function cargarNotas() {
-  if (!fs.existsSync("backend/notas.json")) fs.writeFileSync("backend/notas.json", "[]");
-  return JSON.parse(fs.readFileSync("backend/notas.json", "UTF-8"));
-}
 
-function guardarNotas(notas) {
-  fs.writeFileSync("backend/notas.json", JSON.stringify(notas, null, 2));
-}
+export function guardarnota(data) {
+  let { notas } = data;
 
-export function guardarNota(contenido) {
-  const usuario = obtenerUsuarioActivo();
-  if (!usuario) return { exito: false, mensaje: "No hay usuario activo" };
+  try {
+  let notas = [];
+  notas = fs.writeFileSync('notas.json', JSON.stringify(usuarios, null, 2));
+    notas.push(data)
 
-  const notas = cargarNotas();
-  const nuevaNota = { usuario, contenido, fecha: new Date().toISOString() };
-  notas.push(nuevaNota);
-  guardarNotas(notas);
+  return {exito: true, mensaje: "Nota creada con exito"}
 
-  return { exito: true, mensaje: "Nota guardada" };
-}
-
-export function obtenerNotas() {
-  const usuario = obtenerUsuarioActivo();
-  if (!usuario) return [];
-  const notas = cargarNotas();
-  return notas.filter(n => n.usuario === usuario);
-}
-
-export function borrarNota(indice) {
-  const usuario = obtenerUsuarioActivo();
-  if (!usuario) return { exito: false, mensaje: "No hay usuario activo" };
-
-  const notas = cargarNotas();
-  const notasUsuario = notas.filter(n => n.usuario === usuario);
-
-  if (indice < 0 || indice >= notasUsuario.length) {
-    return { exito: false, mensaje: "Índice inválido" };
+} 
+catch(Error) {
+  console.error("Error al Guardar Nota:", Error);
+  return {exito: false, mensaje: "Error al procesar el archivo"}
   }
-
-  // esto hace que se elimine la nota del usuario activo
-  const notaAEliminar = notasUsuario[indice];
-  const nuevasNotas = notas.filter(n => n !== notaAEliminar);
-
-  guardarNotas(nuevasNotas);
-  return { exito: true, mensaje: "Nota eliminada con éxito" };
 }
-
