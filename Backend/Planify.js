f36bfe96d739713052299f9eb1640b5f89d65119
matias-sciarra <Matiassciarra1@gmail.com>
2025-11-17 15:50:02 -0300

    Evento terminado

--- Backend/Planify.js
+++ Backend/Planify.js
@@ -1,13 +1,16 @@
 import { subscribeGETEvent, subscribePOSTEvent, startServer } from "soquetic";
 import { login, signup } from "./usuarios.js";
 import {guardarnota, borrarnota, cargarnota } from "./notas.js"
+import { guardarevento } from "./eventos.js"
+
 
 
 subscribePOSTEvent("signup", signup);
 subscribePOSTEvent("login", login);
 subscribePOSTEvent("guardarnota", guardarnota);
 subscribePOSTEvent("borrarnota" , borrarnota);
-subscribeGETEvent("cargarnota" , cargarnota)
+subscribeGETEvent("cargarnota" , cargarnota);
+subscribePOSTEvent("guardarevento", guardarevento);
 
 
 startServer(3000, true);
